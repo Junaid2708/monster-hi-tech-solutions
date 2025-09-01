32205e709359653b5f6bf3cb1b33bb590f7108ec
Junaid2708 <115451724+Junaid2708@users.noreply.github.com>
2025-09-01 20:09:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+const scrollBtn = document.getElementById("scrollToTopBtn");
+
+window.onscroll = function () {
+  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+    scrollBtn.style.display = "block";
+  } else {
+    scrollBtn.style.display = "none";
+  }
+};
+
+scrollBtn.onclick = function () {
+  window.scrollTo({
+    top: 0,
+    behavior: 'smooth'
+  });
+};
